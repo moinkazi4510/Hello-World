1494f94d49abe2748959c0ebd33197999e532587
moinkazi4510 <moinkazi4510@gmail.com>
2023-11-28 02:00:02 +0530

    These is my second day of Java code but with different branch name

---
+++ secondDayAlternate.java
@@ -0,0 +1,10 @@
+class secondDayAlternate
+{
+
+public static void main(String args[])
+{
+System.out.println("These is my second day of Java Programming but i want to get some changes obn Day02");
+
+}
+
+}
